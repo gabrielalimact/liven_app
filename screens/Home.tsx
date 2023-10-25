6d35fea066983be0ec4b7107bff0226940a17115
Gabriela Lima <lgcl@ic.ufal.br>
2023-10-25 11:28:25 -0300

    feat: adiciona appbar que filtra por category

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 import {
-  Text, View, Image, StyleSheet, ScrollView
+  Text, View, Image, StyleSheet, ScrollView, Pressable 
 } from 'react-native';
 import { getProducts } from '../api/products';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { COLORS, SIZES } from '../constants';
 
 type Product = {
   category: string,
@@ -20,56 +22,218 @@ type Product = {
 
 const HomeScreen = () => {
   const [data, setData] = useState<Product[]>()
-  const [womensClothing, setWomensClothing] = useState<Product[]>()
-  const [mensClothing, setMensClothing] = useState<Product[]>()
-  const [jewelery, setJewelery] = useState<Product[]>()
-  const [electronics, setElectronics] = useState<Product[]>()
+  const [filterWomensClothing, setFilterWomensClothing] = useState(false)
+  const [filterMensClothing, setFilterMensClothing] = useState(false)
+  const [filterJewelery, setFilterJewelery] = useState(false)
+  const [filterElectronics, setFilterElectronics] = useState(false)
+  const [allProducts, setAllProducts] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     getProducts().then((res) => {
-      const womensClothing = res.filter((item: any) => item.category === 'women\'s clothing')
-      setWomensClothing(womensClothing)
-      
+      const womensClothing = res.filter((item: any) => item.category === 'women\'s clothing')      
       const mensClothing = res.filter((item: any) => item.category === 'men\'s clothing')
-      setMensClothing(mensClothing)
-
       const jewelery = res.filter((item: any) => item.category === 'jewelery')
-      setJewelery(jewelery)
-
       const electronics = res.filter((item: any) => item.category === 'electronics')
-      setElectronics(electronics)
 
       const data = res
-      setData(data)
+      if(allProducts){
+        setData(data)
+        setIsLoading(false)
+      }
+      if(filterWomensClothing){
+        setData(womensClothing)
+      }
+      if(filterMensClothing){
+        setData(mensClothing)
+      }
+      if(filterJewelery){
+        setData(jewelery)
+      }
+      if(filterElectronics){
+        setData(electronics)
+      }
     })
-  }, [])
+  }, [filterElectronics, filterJewelery, filterMensClothing, filterWomensClothing, allProducts])
+
+  const handleFilters = (category: string) => {
+    if(category === 'all'){ 
+      setFilterElectronics(false)
+      setFilterJewelery(false)
+      setFilterMensClothing(false)
+      setFilterWomensClothing(false)
+      setAllProducts(true)
+    }
+    if(category === 'women'){
+      setFilterElectronics(false)
+      setFilterJewelery(false)
+      setFilterMensClothing(false)
+      setAllProducts(false)
+      if(filterWomensClothing){
+        setFilterWomensClothing(false)
+      }else{
+        setFilterWomensClothing(true)
+      }
+    }
+    if(category === 'men'){
+      setFilterElectronics(false)
+      setFilterJewelery(false)
+      setFilterWomensClothing(false)
+      setAllProducts(false)
+      if(filterMensClothing){
+        setFilterMensClothing(false)
+      } else {
+        setFilterMensClothing(true)
+      }
+    }
+    if(category === 'jewelery'){
+      setFilterElectronics(false)
+      setFilterMensClothing(false)
+      setFilterWomensClothing(false)
+      setAllProducts(false)
+      if(filterJewelery){
+        setFilterJewelery(false)
+      } else {
+        setFilterJewelery(true)
+      }
+    }
+    if(category === 'electronics'){
+      setFilterJewelery(false)
+      setFilterMensClothing(false)
+      setFilterWomensClothing(false)
+      setAllProducts(false)
+      if(filterElectronics){
+        setFilterElectronics(false)
+      } else {
+        setFilterElectronics(true)
+      }
+    }
+  }
   
   return (
-    <ScrollView>
-      {data?.map((item, index) => (
-        <View key={index}>
-          <Image
-            source={{ uri: item.image }}
-            style={styles.image}
-          />
-
-          <Text>
-            {item.title}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.textTitle}>
+            Liven's Store
           </Text>
-        
         </View>
-      
-      ))}
+        <ScrollView 
+          horizontal={true} 
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.appBar} >
+          <Pressable onPress={()=>handleFilters('all')} style={[styles.itemAppBar, {
+              backgroundColor: allProducts ? COLORS.primary : COLORS.white,
+            }]}>
+              <Text>
+                All Products
+              </Text>
+            </Pressable>
+            <Pressable onPress={()=>handleFilters('women')} style={[styles.itemAppBar, {
+              backgroundColor: filterWomensClothing ? COLORS.primary : COLORS.white,
+            }]}>
+              <Text>
+                Woman's Clothing
+              </Text>
+            </Pressable>
 
-    
-    </ScrollView>
+            <Pressable onPress={()=>handleFilters('men')} style={[styles.itemAppBar, {
+              backgroundColor: filterMensClothing ? COLORS.primary : COLORS.white,
+            }]}>
+              <Text>
+                Men's Clothing
+              </Text>
+            </Pressable>
+
+            <Pressable onPress={()=>handleFilters('jewelery')} style={[styles.itemAppBar, {
+              backgroundColor: filterJewelery ? COLORS.primary : COLORS.white,
+            }]}>
+              <Text>
+                Jewelery
+              </Text>
+            </Pressable>
+
+            <Pressable onPress={()=>handleFilters('electronics')} style={[styles.itemAppBar, {
+              backgroundColor: filterElectronics ? COLORS.primary : COLORS.white,
+            }]}>
+              <Text>
+                Electronics
+              </Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+        {isLoading ? (
+          <Text>Loading...</Text> 
+        ) : (
+          <View style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+          }}>
+
+            {data?.map((item, index) => (
+              <View key={index} style={styles.viewProducts}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.image}
+                  />
+                <Text>
+                  {item.title}
+                </Text>
+              </View>
+            ))}
+          </View>
+        )}
+        
+
+      
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: SIZES.large,
+    marginTop: SIZES.small,
+    backgroundColor: COLORS.offwhite,
+    marginBottom: SIZES.xxLarge,
+  },
+  titleView: {
+    marginBottom: SIZES.small,
+  },
   image: {
-    width: 100,
-    height: 100
+    width: 180,
+    height: 180,
+    marginBottom: SIZES.small,
+    resizeMode: 'contain'
+  },
+  textTitle: {
+    fontFamily: 'bold',
+    fontSize: 30,
+    textAlign: 'left',
+  },
+  appBar: {
+    flexDirection: 'row',
+    marginBottom: SIZES.small,
+    padding: 10,
+    alignItems: 'center',
+
+  },
+  itemAppBar: {
+    marginRight: 15,
+    fontFamily: 'regular',
+    fontSize: 16,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: COLORS.white,
+  },
+  viewProducts: {
+    width: '45%',
+    alignItems: 'center',
+    marginBottom: SIZES.medium,
   }
 });
 
