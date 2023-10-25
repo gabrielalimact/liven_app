5475831ad64b4b23ffb95a5aff6b436bd7e01ec0
Gabriela Lima <lgcl@ic.ufal.br>
2023-10-25 11:29:08 -0300

    update libs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreehn from 'expo-splash-screen';
 import { useCallback } from 'react';
@@ -42,16 +40,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textStyle: {
-    fontFamily: 'regular',
-    fontSize: 16,
-  }
-});
